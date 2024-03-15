b75e6f52b58b015f3ac5dc3d826dee8ac2ecf0d6
thanwin84 <thanwinhline84@gmail.com>
2024-03-15 17:14:46 +0600

    Add Controllers in following.controller.js
    Added Getfollowings and getFollowersAndFollowingCount Controller

--- src/controllers/following.controller.js
+++ src/controllers/following.controller.js
@@ -1,6 +1,5 @@
 import {asyncHandler} from '../utils/asyncHandler.js'
 import {ApiError} from '../utils/ApiError.js'
-import {Blog} from '../models/blog.model.js'
 import { User } from '../models/user.model.js'
 import { Following } from '../models/following.model.js'
 import {ApiResponse} from '../utils/ApiResponse.js'
@@ -74,18 +73,18 @@ const getFollowers = asyncHandler(async (req, res)=>{
     // check how many followers a blogger has
     const {limit = 10, page = 1} = req.query
     const skips = (Number(page) - 1) * Number(limit)
-    const {userId}= req.params
+    const {bloggerId}= req.params
    
-    if (!userId){
+    if (!bloggerId){
         throw new ApiError(
             httpStatusCodes.BAD_REQUEST,
-            "User id is missing "
+            "blogger id is missing "
         )
     }
     const aggregationPipeline = [
         {
           $match: {
-            followedBlogger: new mongoose.Types.ObjectId(userId)
+            followedBlogger: new mongoose.Types.ObjectId(bloggerId)
           }
         },
         {
@@ -148,12 +147,127 @@ const getFollowers = asyncHandler(async (req, res)=>{
     }
 })
 
-const getFollwingUser = asyncHandler(async (req, res)=>{
-    
+const getFollwings = asyncHandler(async (req, res)=>{
+    // get the list of following users
+    const {userId} = req.params
+    const {limit = 10, page = 1} = req.query
+    const skips = (Number(page) - 1) * Number(limit)
+
+    if (!userId){
+        throw new ApiError(
+            httpStatusCodes.BAD_REQUEST,
+            "user id is missing"
+        )
+    } 
+    const user = await User.findById(userId)
+    if (!user){
+        throw new ApiError(
+            httpStatusCodes.OK,
+            "use does not exist"
+        )
+    }
+    const aggregationPipeline = [
+        {
+          $match: {
+            follower: new mongoose.Types.ObjectId(userId)
+          }
+        },
+        {
+          $project: {
+            followedBlogger: 1,
+            _id: 0
+          }
+        },
+        {
+          $lookup: {
+            from: "users",
+            localField: "followedBlogger",
+            foreignField: "_id",
+            as: "user",
+            pipeline: [
+                {
+                  $project: {
+                    _id: 1,
+                    username: 1,
+                    avatar: 1
+                  }
+                }
+              ]
+          }
+        }, 
+        {
+          $addFields: {
+            user: {$first: "$user"}
+          }
+        },
+        {
+          $project: {
+            user: 1
+          }
+        },
+        {
+            $skip: skips
+        },
+        {
+            $limit: Number(limit)
+        }
+      ]
+   
+    try {
+        const followings = await Following.aggregate(aggregationPipeline)
+
+        return res
+        .status(httpStatusCodes.OK)
+        .json(new ApiResponse(
+            httpStatusCodes.OK,
+            followings,
+            "List of followings has been fetched successfully"
+        ))
+    } catch (error) {
+        throw new ApiError(
+            httpStatusCodes.INTERNAL_SERVER_ERROR,
+            "Something went wrong while fetching following list"
+        )
+    }
+
 })
 
-
+const getFollowersAndFollowingCount = asyncHandler(async (req, res)=>{
+    const {userId} = req.params
+    if (!userId){
+        throw new ApiError(
+            httpStatusCodes.BAD_REQUEST,
+            "user id is missing"
+        )
+    }
+    const user = await User.findById(userId)
+    if (!user){
+        throw new ApiError(
+            httpStatusCodes.BAD_REQUEST,
+            "user does not exist"
+        )
+    }
+    try {
+        const followersCount = await Following.find({followedBlogger: userId}).countDocuments()
+        const followingCount = await Following.find({follower: userId}).countDocuments()
+        
+        return res
+        .status(httpStatusCodes.OK)
+        .json(new ApiResponse(
+            httpStatusCodes.OK,
+            {followersCount, followingCount},
+            "Followers  and following count has been fetched successsfully"
+        ))
+    } catch (error) {
+        throw new ApiError(
+            httpStatusCodes.BAD_REQUEST,
+            "Something went wrong while fetching"
+        )
+    }
+})
 export {
     toggleFollowing,
-    getFollowers
+    getFollowers,
+    getFollwings,
+    getFollowersAndFollowingCount
 }
--- src/routes/following.route.js
+++ src/routes/following.route.js
@@ -1,20 +1,23 @@
 import { Router } from "express";
 import {
     toggleFollowing,
-    getFollowers
+    getFollowers,
+    getFollwings,
+    getFollowersAndFollowingCount
 } from '../controllers/following.controller.js'
 import {verityJWT} from '../middlewares/verityJWT.middleware.js'
 
 const router = Router()
 
 /// public route
-router.route("/users/:userId")
-.get(getFollowers)
+router.route("/:userId").get(getFollwings)
+router.route("/followers/:bloggerId").get(getFollowers)
+router.route("/followersAndFollowings/count/:userId").get(getFollowersAndFollowingCount)
 
 // protected route
 router.use(verityJWT)
 
-router.route("/:bloggerId").post(toggleFollowing)
+router.route("/follow/:bloggerId").post(toggleFollowing)
 
 
 export default router;
